a37f9d92aaafb12f98f982014d58ec18bcf6a931
manuel-coder 02 <sotundeemmanuel@gmail.com>
2024-12-22 01:59:17 +0100

    80% done with the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     extend: {
       colors: {
         blue: "hsl(228, 45%, 44%)",
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
       },
     },
   },
